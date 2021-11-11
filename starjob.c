639d3cfc879059c2c358ba121f9891c6afab3bcd
r9822501 <r9822501@gmail.com>
2021-11-11 11:21:02 +0200

    ניסוי

---
+++ starjob.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+// Shirel Ghanah , ID : 206645103 //
+{
+	char letter;
+	printf("please enter a latinic letter. \n");
+	scanf("%c", &letter);
+
+	if ((letter >= 'A') && (letter <= 'Z')) // checking the first term //  
+		letter = letter + 32;
+	else if ((letter >= 'a') && (letter <= 'z')) // checking the second term // 
+		letter = letter - 32;
+	else
+		letter = letter;
+
+	printf("The new letter is : %c . \n ", letter); // printing //
+
+	return 0;
+}
+
