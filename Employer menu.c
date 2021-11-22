4b699d022ee60a733ca820cda49e32d8caa33848
adibuch <92858769+adibuch@users.noreply.github.com>
2021-11-22 21:20:29 +0200

    פוקנציות של צפייה בפרופיל ןעריכת פרופיל מעסיק

--- Employer menu.c
+++ Employer menu.c
@@ -2,6 +2,92 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+void EmployerProfile(Employer* employer)
+{
+	int choice;
+	do
+	{
+		//printDate(employer.birthday);
+		printf("%s\n%ld\n%ld\n%s\n%s\n%s\n", employer->name, employer->phoneNumber, employer->id, employer->email, employer->companyName, employer->companyDescription);//חסר הדפסת תאריך לידה
+		printf("1. Edit Profile\n0. Back to the previous menu\n");
+		scanf("%d", &choice);
+		switch (choice)
+		{
+		case 1:
+			EditProfile(employer);
+			break;
+		case 0:
+			return;
+
+		default:
+			printf("Error,try again.\n");
+			break;
+		}
+
+	} while (choice != 0);
+}
+
+void EditProfile(Employer* e)
+{
+	int choice;
+	char name[N];
+	//Date birthday;
+	long id;
+	char email[N];
+	char companyName[N];
+	char companyDescription[M];
+	long phoneNumber;
+
+	do
+	{
+
+		//הדפסת פרטי משתמש של שם ת.ז ...
+		printf("1. Full name\n 2. Email address\n 3.phone number\n 4. company name\n5. general description\n6.Back to the previous menu");
+		scanf("%d", &choice);
+		switch (choice)
+		{
+		case 1:
+			printf("Enter your name:\n");
+			getchar();
+			gets_s(name, N);
+			//בדיקת תקינות קלט
+			strcpy(e->name, name);
+
+			break;
+		case 2:
+
+
+			break;
+		case 3:
+			printf("Enter your phone:\n");
+			scanf("%ld", &phoneNumber);
+			//בדיקת תקינות קלט
+			e->phoneNumber = phoneNumber;
+			break;
+		case 4:
+			printf("Enter your name company:\n");
+			getchar();
+			gets_s(companyName, N);
+			//בדיקת תקינות קלט
+			strcpy(e->companyName, companyName);
+			break;
+		case 5:
+			printf("Enter a description of your company :\n");
+			getchar();
+			gets_s(companyDescription, M);
+			//בדיקת תקינות קלט
+			strcpy(e->companyDescription, companyDescription);
+			break;
+		case 6:
+			return;
+		default:
+			printf("Error,try again.\n");
+			break;
+		}
+
+
+	} while (choice != 6);
+}
 
 void EmployerMenu()
 
