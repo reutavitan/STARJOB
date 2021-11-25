b969a57d4596f5b377ee340c7cd4187e807213f3
adibuch <92858769+adibuch@users.noreply.github.com>
2021-11-25 19:09:09 +0200

    עדכון של הקבצים . צרפו את הקיראה והכתיבה של המשרות

---
+++ view job employer.cpp
@@ -0,0 +1,205 @@
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include<malloc.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 20
+#define SIZE2 500
+typedef enum { employer = 0, candidate = 1 }Gender;
+typedef enum { FALSE = 0, TRUE = 1 }Bool;
+typedef struct {
+	int day, month, year;
+}Date;
+typedef struct {
+	char id[SIZE];
+	char password[SIZE];
+	char fullName[SIZE];
+	char email[SIZE];
+	char phoneNumber[SIZE];
+	char education[SIZE];
+	char workExperience[SIZE];
+	//קורות חיים 
+	Date birthDate;
+	Gender gender;
+}Candidate;
+typedef struct {
+	char id[SIZE];
+	char password[SIZE];
+	char fullName[SIZE];
+	char email[SIZE];
+	char phoneNumber[SIZE];
+	char companyMame[SIZE];
+	char description[SIZE];
+	Date birthDate;
+	Gender gender;
+}Employer;
+typedef enum { Fulltime = 1, Parttime = 2, Temporaryjob = 3, Motherjob = 4 }Scope;
+typedef enum { North = 1, South = 2, Central = 3, Jerusalem = 4 }Area;
+typedef enum { Sales = 1, AccountingAndFinance = 2, Logistics = 3, EducationandTraining = 4, HiTech = 5, Securing = 6, Restaurants = 7 }Profession;
+typedef struct {
+	char id[SIZE];
+	char companyName[SIZE];
+	char name[SIZE];
+	Bool status;
+	Scope  scope;
+	Area  area;
+	Profession profession;
+	char description[SIZE];
+	char requirements[SIZE];
+}Job;
+
+
+
+void viewAllJobOfEmployer(Employer* employer, Job** job, int size)
+{
+	int choise;
+	do
+	{
+		for (int i = 0; i < size; ++i)
+		{
+			if (job[i]->id == employer->id && job[i]->status == TRUE)
+				printJob(job[i]);
+
+		}
+
+		printf("Enter a job ID number to edit a job / delete a job / view job applicants\nPress 0 to return to the main menu\n");
+		scanf("%d", &choise);
+		if (choise == 0)
+			return;
+		menuJob(job[choise]);
+
+
+	} while (choise != 0);
+
+
+}
+int menuJob(Job* job)
+{
+	int choice;
+	do
+	{
+		printf("1) Option to edit a job\n2) Option to delete a job\n3) List of candidates for the position\n0)back to the job list published by the employer.\n");
+		scanf("%d", &choice);
+		switch (choice)
+		{
+		case 1:
+			editJob(job);
+
+			break;
+		case 2:
+			deleteJob(job);
+			break;
+		case 3:
+			viewListOfCandidatesForThePosition();//לשאול איך אנחנו מציגים רשימת מועמדים ועוברים לפרופיל
+			break;
+		case 0:
+			return 0;
+
+		default:
+			printf("Error,try again.\n");
+			break;
+		}
+
+	} while (choice != 0);
+
+}
+void viewListOfCandidatesForThePosition()
+{
+
+}
+void editJob(Job* job)
+{
+	int choice;
+	char jobName[SIZE];
+	char companyName[SIZE];
+	enum scope scope;
+	enum positionArea area;
+	char jobDescription[SIZE2];
+	char requirements[SIZE2];
+	enum profession profession;
+
+	do
+	{
+
+		//הדפסת פרטי משתמש של שם ת.ז ...
+		printf("1. Job name\n 2. Company name\n 3.Scope\n 4.position area\n5.General description of position\n6.Job requirements\n7.Profession\n8.Back to the previous menu\n");
+		scanf("%d", &choice);
+		switch (choice)
+		{
+		case 1:
+			printf("Enter job name:\n");
+			getchar();
+			gets_s(jobName, N);
+			//בדיקת תקינות קלט
+			strcpy(job->name, jobName);
+
+			break;
+		case 2:
+			printf("Enter Company name:\n");
+			getchar();
+			gets_s(companyName, N);
+			//בדיקת תקינות קלט
+			strcpy(job->companyName, companyName);
+
+
+			break;
+		case 3:
+			//	Scope-קריאה לפונקציה שממשת בחירה של היקף משרה
+			break;
+		case 4:
+			//Area 
+
+			break;
+		case 5:
+			printf("Enter a description of job :\n");
+			getchar();
+			gets_s(jobDescription, M);
+			//בדיקת תקינות קלט
+			strcpy(job->description, jobDescription);
+			break;
+		case 6:
+			printf("Enter the job requirements :\n");
+			getchar();
+			gets_s(requirements, M);
+			//בדיקת תקינות קלט
+			strcpy(job->requirements, requirements);
+			break;
+
+		case 7:
+			//תחום, מקצוע
+			break;
+		case 8:
+			return;
+
+		default:
+			printf("Error,try again.\n");
+			break;
+
+		}
+
+
+	} while (choice != 8);
+
+}
+void deleteJob(Job* job)
+{
+	if (job->status)
+	{
+		job->status = FALSE;
+		printf("The job was deleted.\n");
+	}
+}
+void printJob(Job* job)
+{
+	//הדפסה של משתנה בולאיני??
+	printf("The name of job:\n%s\nCompany Name:\n%s\nDescription of the company:\n%s\nRequirements:\n%s\nArea:\n%", job->name, job->companyName, job->description, job->requirements, job->area, job->profession, job->scope, job->id);
+}
+int main() {
+	Candidate** candidateDataBase = (Candidate**)malloc(sizeof(Candidate*) * SIZE);
+	Employer** employerDataBase = (Employer**)malloc(sizeof(Employer*) * SIZE);
+	Job** jobDataBase = (Job**)malloc(sizeof(Job*) * SIZE);
+	Bool fillDefaultCandidateDataBase(Candidate * *candidateDataBase); {
+
+	};
+};
---
+++ write and read file.c
@@ -0,0 +1,169 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include<malloc.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 20
+#define SIZE2 500 //for free text
+sizeEmployer = 0;
+sizeCandidate = 0;
+sizeJob = 0;
+//typedef enum { employer = 0, candidate = 1 }Gender;
+typedef enum { FALSE = 0, TRUE = 1 }Bool;
+typedef struct {
+	int day, month, year;
+}Date;
+typedef struct {
+	char id[SIZE2];
+	char password[SIZE2];
+	char fullName[SIZE2];
+	char phoneNumber[SIZE2];
+	char education[SIZE2];
+	char workExperience[SIZE2];
+	char gender[SIZE2];
+	char CV[SIZE2];//קורות חיים
+}Candidate;
+typedef struct {
+	char id[SIZE2];
+	char password[SIZE2];
+	char fullName[SIZE2];
+	char phoneNumber[SIZE2];
+	char companyName[SIZE2];
+	char description[SIZE2];
+	char gender[SIZE2];
+}Employer;
+typedef enum { Fulltime = 1, Parttime = 2, Temporaryjob = 3, Motherjob = 4 }Scope;
+typedef enum { North = 1, South = 2, Central = 3, Jerusalem = 4 }Area;
+typedef enum { Sales = 1, AccountingAndFinance = 2, Logistics = 3, EducationandTraining = 4, HiTech = 5, Securing = 6, Restaurants = 7 }Profession;
+typedef struct {
+	char id[SIZE];
+	char companyName[SIZE];
+	char name[SIZE];
+	Scope  scope;//קליטה של מספר לקובץ
+	Area  area;
+	Profession profession;
+	char description[SIZE];
+	char requirements[SIZE];
+	bool status;//0-avalabe 1- ananabala
+	char candidate1[SIZE2];
+	char candidate2[SIZE2];
+	char candidate3[SIZE2];
+}Job;
+int writeFileEmployer(Employer** employer, int size)
+{
+	int i;
+	FILE* fp = fopen("employer.txt", "w");
+	for (i = 0; i < size&&; i++)
+		fprintf(fp, "%s\n%s\n%s\n%s\n%s\n%s\n%s\n", employer[i]->id, employer[i]->password, employer[i]->fullName, employer[i]->phoneNumber, employer[i]->companyName, employer[i]->description, employer[i]->gender);
+	fclose(fp);
+	return 1;
+}
+int writeFileCandidate(Candidate** candidate, int size)
+{
+	int i;
+	FILE* fp = fopen("Candidate.txt", "w");
+	for (i = 0; i < 2; i++)
+		fprintf(fp, "%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n", candidate[i]->id, candidate[i]->password, candidate[i]->fullName, candidate[i]->phoneNumber, candidate[i]->education, candidate[i]->workExperience, candidate[i]->gender, candidate[i]->CV);
+	fclose(fp);
+	return 1;
+}
+int readFileEmployer(Employer** employer, int size)
+{
+	int i;
+	FILE* fp = fopen("employer.txt", "r");
+	for (i = 0; i < size; i++) {
+		//x=fscanf(fp, "%s\n%s\n%s\n%s\n%s\n%s\n", employer[i]->fullName, employer[i]->companyName, employer[i]->id, employer[i]->password, employer[i]->phoneNumber, employer[i]->gender);
+		fgets(employer[i]->id, SIZE2, fp);
+		fgets(employer[i]->password, SIZE2, fp);
+		fgets(employer[i]->fullName, SIZE2, fp);
+		fgets(employer[i]->phoneNumber, SIZE2, fp);
+		fgets(employer[i]->companyName, SIZE2, fp);
+		fgets(employer[i]->description, SIZE2, fp);
+		fgets(employer[i]->gender, SIZE2, fp);
+	}
+	fclose(fp);
+	for (i = 0; i < 2; i++)
+	{
+		printf("%s%s%s%s%s%s%s", employer[i]->id, employer[i]->password, employer[i]->fullName, employer[i]->phoneNumber, employer[i]->companyName, employer[i]->description, employer[i]->gender);
+	}
+
+}
+int readFileCandidate(Candidate** candidate, int size)
+{
+	int i;
+	FILE* fp = fopen("Candidate.txt", "r");
+	for (i = 0; i < 2; i++)
+	{ /*x = fscanf(fp, "%s\n%s\n%s\n%s\n%s\n%s\n%s\n", candidate[i]->id, candidate[i]->fullName, candidate[i]->password, candidate[i]->phoneNumber, candidate[i]->education, candidate[i]->workExperience, candidate[i]->CV);*/
+		fgets(candidate[i]->id, SIZE2, fp);
+		fgets(candidate[i]->password, SIZE2, fp);
+		fgets(candidate[i]->fullName, SIZE2, fp);
+		fgets(candidate[i]->phoneNumber, SIZE2, fp);
+		fgets(candidate[i]->education, SIZE2, fp);
+		fgets(candidate[i]->workExperience, SIZE2, fp);
+		fgets(candidate[i]->gender, SIZE2, fp);
+		fgets(candidate[i]->CV, SIZE2, fp);
+	}
+
+	fclose(fp);
+
+	for (i = 0; i < 2; i++)
+	{
+		printf("%s%s%s%s%s%s%s%s", candidate[i]->id, candidate[i]->fullName, candidate[i]->password, candidate[i]->phoneNumber, candidate[i]->education, candidate[i]->workExperience, candidate[i]->gender, candidate[i]->CV);
+	}
+
+}
+int main() {
+	Employer** employer = (Employer**)malloc(2 * sizeof(Employer*));
+	Candidate** candidate = (Candidate**)malloc(2 * sizeof(Candidate*));
+	int i;
+
+	for (i = 0; i < 2; i++)
+	{
+		puts("enter all datils\n");
+		employer[i] = (Employer*)malloc(1 * sizeof(Employer));
+		if (!employer[i])
+			return 0;
+		gets(employer[i]->id);
+		gets(employer[i]->password);
+		gets(employer[i]->fullName);
+		gets(employer[i]->phoneNumber);
+		gets(employer[i]->companyName);
+		gets(employer[i]->description);
+		gets(employer[i]->gender);
+	}
+
+
+
+	for (i = 0; i < 2; i++)
+	{
+		puts("enter all datils\n");
+		candidate[i] = (Candidate*)malloc(1 * sizeof(Candidate));
+		if (!candidate[i])
+			return 0;
+		gets(candidate[i]->id);
+		gets(candidate[i]->password);
+		gets(candidate[i]->fullName);
+		gets(candidate[i]->phoneNumber);
+		gets(candidate[i]->education);
+		gets(candidate[i]->workExperience);
+		gets(candidate[i]->gender);
+		gets(candidate[i]->CV);
+		/*x = scanf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n", candidate[i]->id, candidate[i]->fullName, candidate[i]->password, candidate[i]->phoneNumber, candidate[i]->education, candidate[i]->workExperience, candidate[i]->CV);*/
+
+	}
+	writeFileEmployer(employer, 2);
+	readFileEmployer(employer, 2);
+	writeFileCandidate(candidate, 2);
+	readFileCandidate(candidate, 2);
+
+	/*FILE* fp = fopen("arr.txt", "w");
+	for (i = 0; i < 2; i++)
+		fprintf(fp, "%s %d %f\n", arr[i]->name, arr[i]->age, arr[i]->price);
+	fclose(fp);*/
+	/*readFileEmployer(employer, 2);*/
+
+	return 0;
+
+}
